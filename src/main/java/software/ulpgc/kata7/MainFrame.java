0c0bb68fed390845b27e617fdba5ed2d636b962a
Jesus Alberto Ortega Hernández <jesus.ortega103@alu.ulpgc.es>
2024-01-02 15:15:21 +0000

    Creation of class MainFrame. Contains the code for the basic application.

--- src/main/java/software/ulpgc/kata7/MainFrame.java
+++ src/main/java/software/ulpgc/kata7/MainFrame.java
@@ -1,4 +1,39 @@
 package software.ulpgc.kata7;
 
-public class MainFrame {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class MainFrame extends JFrame implements ActionListener {
+    private JButton jButton;
+    private  JPanel jPanel;
+    private Random coin = new Random();
+    private JLabel jLabel;
+    public MainFrame() throws HeadlessException{
+        jButton = new JButton("Click for flip.");
+        jButton.setSize(200, 40);
+        jButton.addActionListener(this);
+        this.add(jButton);
+
+        jLabel = new JLabel();
+        jLabel.setSize(100, 100);
+        this.add(jLabel);
+
+        this.setTitle("Coin flip");
+        this.setSize(800, 600);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        int a = coin.nextInt(0, 2);
+        if (a == 1){
+            jLabel.setText("Head");
+        }
+        else if(a == 0){
+            jLabel.setText("Tail");
+        }
+    }
 }
